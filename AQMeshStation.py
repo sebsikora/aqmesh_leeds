1d59b38a08d121d3d46a7cfd470f77c34d7f3ede
Seb Sikora <menssi@leeds.ac.uk>
2019-04-27 23:23:36 +0100

    Completely reworked comms send and receive code to use start and end markers for messages followed by a 'Maxim style' CRC8 check-byte. All transfers now fail if they do not pass the check. Need to write 'spooling' code, to send new data files triggered by a message from the RPI.

--- AQMeshStation.py
+++ AQMeshStation.py
@@ -2,79 +2,96 @@ import urllib
 import ntplib
 from time import ctime
 import datetime
+import time
 
 import ArduinoComms
 
 class AQMeshStation():
 	def __init__ (self):
-		self.MAX_COMMAND_RETRIES = 5
+		self.MAX_COMMAND_RETRIES = 10
+		self.MAX_PARAMETER_RETRIES = 5
+		self.MAX_TIMESERVER_RETRIES = 5
+		
+		self.arduino_port = '/dev/ttyACM0'
+		self.arduino_baud = 115200
+		self.arduino_timeout_secs = 1.0
 		
 		# Block until we detect a working web connection.
 		while (not self.internetOn()):
 			pass
 		
-		# Connect to a NTP server and query the current time
-		timestamp = self.getNtpTime()
-		print timestamp
-		
 		# Start the serial connection with the arduino.
-		self.arduino = ArduinoComms.ArduinoComms(57600, '/dev/ttyACM0', 0.5)
+		comms_success = self.startComms()
 		
 		# Set the time on the arduino RTC to that returned from the NTP server.
-		self.setTime(timestamp)
+		comms_success, completed = self.setTime()
+		print completed
 	
-	def setTime(self, timestamp):
-		response = ['']
-		tries = 0
-		while ((response[0] != 'ack') and (tries < self.MAX_COMMAND_RETRIES)):
-			response = self.arduino.Call('Settime', 1)
-			tries += 1
-			print response
-		
-		# ----------- Simulating comms glitch, command is sent again when arduino expects a time header ------------------
-		# Send the erroneous command...
-		print self.arduino.Call('Settime', 1)
-		response = ['']
-		# The arduino should have switched back to command mode - try sending the command again...
-		while ((response[0] != 'ack') and (tries < self.MAX_COMMAND_RETRIES)):
-			response = self.arduino.Call('Settime', 1)
-			tries += 1
-			print response
-		# ----------------------------------------------------------------------------------------------------------------
-		
-		time_headers = ['YY', 'MM', 'DD', 'hh', 'mm', 'ss']
+	def setTime(self):
 		completed = False
-		for i, parameter in enumerate(timestamp):
-			response = ['']
-			tries = 0
-			while ((response[0] != 'hit') and (tries < self.MAX_COMMAND_RETRIES)):
-				response = self.arduino.Call(time_headers[i], 1)
-				tries += 1
-				print response
-			
-			#~ # ----------- Simulating comms glitch, time header is sent again when arduino expects a time value -------------
-			#~ if i == 0:
-				#~ # Send the erroneous header...
-				#~ print self.arduino.Call('YY', 1)
-				#~ response = ['']
-				#~ # The arduino should have switched back to header mode - try sending the header again...
-				#~ while ((response[0] != 'hit') and (tries < self.MAX_COMMAND_RETRIES)):
-					#~ response = self.arduino.Call('YY', 1)
-					#~ tries += 1
-					#~ print response
-			#~ # --------------------------------------------------------------------------------------------------------------
-			
-			response = ['']
-			tries = 0
-			while ((response[0] != 'hit') and (tries < self.MAX_COMMAND_RETRIES)):
-				response = self.arduino.Call(str(parameter), 1)
-				tries += 1
-				print response
-				if response[0] == 'Time set.':
-					completed = True
-					break
-			if completed == True:
+		# Check if serial connection to arduino is still open.
+		try:
+			self.arduino.ser.isOpen()
+			comms_success = True
+		except:
+			# If not, setTime fails.
+			print 'Could not open serial connection with Arduino...'
+			comms_success = False
+			return comms_success, completed
+		tries = 0
+		while ((completed == False) and (tries < self.MAX_COMMAND_RETRIES)):
+			# Connect to a NTP server and query the current time
+			got_timestamp, timestamp = self.getNtpTime()
+			if got_timestamp == False:
+				print 'Unable to obtain NTP timestamp.'
 				break
+			print timestamp
+			response = self.arduino.Call('ST', 1)
+			print response
+			reply = response[0][1]
+			crc_success = response[0][0]
+			if ((reply == 'ak') and (crc_success == True)):
+				time_headers = ['YY', 'MM', 'DD', 'hh', 'mm', 'ss']
+				for i, parameter in enumerate(timestamp):
+					tries = 0
+					while tries < self.MAX_PARAMETER_RETRIES:
+						response = self.arduino.Call(time_headers[i], 1)
+						print response
+						reply = response[0][1]
+						crc_success = response[0][0]
+						if ((reply == 'ht') and (crc_success == True)):
+							response = self.arduino.Call(str(parameter), 1)
+							print response
+							reply = response[0][1]
+							crc_success = response[0][0]
+							if ((reply == 'TS') and (crc_success == True)):
+								completed = True
+								break
+							elif ((reply == 'ht') and (crc_success == True)):
+								break
+							else:
+								tries += 1
+						else:
+							tries += 1
+			else:
+				tries += 1
+		return comms_success, completed
+	
+	def startComms(self):
+		# Start the serial connection with the arduino.
+		try:
+			self.arduino = ArduinoComms.ArduinoComms(self.arduino_baud, self.arduino_port, self.arduino_timeout_secs)
+			time.sleep(1.0)
+			return True
+		except:
+			return False
+	
+	def calculateLRC(self, parameter_string):
+		# Calculate a simple left-to-right xor checksum of the parameter value as received.
+		lrc = 0;
+		for current_character in parameter_string:
+			lrc = lrc ^ ord(current_character)
+		return lrc
 	
 	def internetOn(self):
 		print 'Checking for working internet connection...'
@@ -89,14 +106,16 @@ class AQMeshStation():
 	def getNtpTime(self):
 		print 'Grabbing time...'
 		c = ntplib.NTPClient()
-		
-		while(True):
+		tries = 0
+		completed = False
+		while((tries < self.MAX_TIMESERVER_RETRIES) and (completed == False)):
 			try:
 				response = c.request('europe.pool.ntp.org', version = 3)
-				print 'Time obtained from europe.pool.ntp.org'
-				break
+				print 'NTP timestamp obtained from europe.pool.ntp.org'
+				completed = True
 			except:
 				print 'No response received from europe.pool.ntp.org.'
+				tries += 1
 		
 		datetime_string = ctime(response.tx_time)
 		datetime_object = datetime.datetime.strptime(datetime_string, '%a %b %d %H:%M:%S %Y')
@@ -107,7 +126,7 @@ class AQMeshStation():
 		hour = datetime_object.hour
 		minute = datetime_object.minute
 		second = datetime_object.second
-		return [year, month, day, hour, minute, second]
+		return completed, [year, month, day, hour, minute, second]
 
 if __name__ == '__main__':
 	aqmeshstation = AQMeshStation()
--- ArduinoComms.py
+++ ArduinoComms.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import crcmod.predefined
 
 class ArduinoComms():
 	def __init__(self, baud, port, timeout_secs):
@@ -7,49 +8,82 @@ class ArduinoComms():
 		self.port = port
 		self.timeout = timeout_secs
 		self.ser = serial.Serial(self.port, self.baud)
+		time.sleep(1.0)
 	
 	def __SerialSpeak(self, message):
 		try:
-			message = '>' + message + '\n'
+			message = '>' + message + '<' + str(self.calcCRC8(message)) + '\0'
 		except:
-			message = '>' + str(message) + '\n'
+			message = '>' + str(message) + '<' + str(self.calcCRC8(str(message))) + '\0'
 		print message
 		for current_character in message:
 			self.ser.write(current_character)
 	
 	def __SerialListen(self, timeout):
-		timeout_start = time.time()
 		returned_message = ''
+		crc_check_string = ''
+		crc_success = False
+		mode = 0
+		started = False
 		finished = False
 		if timeout == 0.0:
 			while finished == False:
 				while ((self.ser.inWaiting() > 0)):
-					received_character = self.ser.read()
-					if received_character == '\n':
-						finished = True
-						break
+					received_character = self.ser.read(1)
+					if received_character == '>':
+						started = True
+					elif received_character == '<':
+						mode = 1
+					elif received_character == '\0':
+						if started == True:
+							finished = True
+							break
 					else:
-						returned_message += received_character
+						if mode == 0:
+							returned_message += received_character
+						elif mode == 1:
+							crc_check_string += received_character
 		else:
+			mode = 0
 			started = False
+			timeout_start = time.time()
 			while time.time() < timeout_start + timeout:
-				received_character = self.ser.read()
-				if received_character == '>':
-					started = True
-				elif received_character == '\n':
-					if started == True:
-						break
-				else:
-					if started == True:
-						returned_message += received_character
-		return returned_message
+				if self.ser.inWaiting() > 0:
+					received_character = self.ser.read(1)
+					if received_character == '>':
+						started = True
+					elif received_character == '<':
+						mode = 1
+					elif received_character == '\0':
+						if started == True:
+							break
+					else:
+						if started == True:
+							if mode == 0:
+								returned_message += received_character
+							elif mode == 1:
+								crc_check_string += received_character
+		try:
+			crc_check_value = int(crc_check_string)
+			message_crc_check_value = self.calcCRC8(returned_message)
+			print 'CRC8 check', crc_check_value, message_crc_check_value
+			if crc_check_value == message_crc_check_value:
+				crc_success = True
+		except:
+			crc_success = False
+		return crc_success, returned_message
 	
 	def Call(self, message, expected_replies):
-		self.ser.flushInput()
-		self.__SerialSpeak(message)
 		response = []
+		self.__SerialSpeak(message)
 		for reply in range(expected_replies):
-			temp = self.__SerialListen(self.timeout)
-			response.append(temp)
+			crc_success, reply_message = self.__SerialListen(self.timeout)
+			response.append([crc_success, reply_message])
 		return response
+	
+	def calcCRC8(self, message):
+		crc8 = crcmod.predefined.mkPredefinedCrcFun('crc-8-maxim')
+		crc8_check_value = crc8(message)
+		return crc8_check_value
+		
 		
